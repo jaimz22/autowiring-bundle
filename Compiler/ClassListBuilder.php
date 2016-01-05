1b7926249ee0ca4601afe9e0bec263a9b410ae74
jaimz22 <jaimz22@users.noreply.github.com>
2016-01-05 12:09:04 -0500

    passing ignored services regex to class list builder

--- Compiler/ClassListBuilder.php
+++ Compiler/ClassListBuilder.php
@@ -1,49 +1,79 @@
 <?php
 
-namespace Kutny\AutowiringBundle\Compiler;
+namespace Kutny\AutowiringBundle;
 
+use Kutny\AutowiringBundle\Compiler\ClassConstructorFiller;
+use Kutny\AutowiringBundle\Compiler\ClassListBuilder;
 use ReflectionClass;
+use Symfony\Component\Config\Resource\FileResource;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 
-class ClassListBuilder
+class AutowiringCompilerPass implements CompilerPassInterface
 {
 
-    public function buildClassList(ContainerBuilder $containerBuilder)
+    private $classConstructorFiller;
+    private $classListBuilder;
+
+    public function __construct(ClassConstructorFiller $classConstructorFiller, ClassListBuilder $classListBuilder)
     {
-        $classes = array();
-        $parameterBag = $containerBuilder->getParameterBag();
+        $this->classConstructorFiller = $classConstructorFiller;
+        $this->classListBuilder = $classListBuilder;
+    }
 
-        foreach ($containerBuilder->getDefinitions() as $serviceId => $definition) {
-            $class = $parameterBag->resolveValue($definition->getClass());
+    public function process(ContainerBuilder $containerBuilder)
+    {
+        $ignoredServicesRegExp = $this->getIgnoredServicesRegExp($containerBuilder);
+        $classList = $this->classListBuilder->buildClassList($containerBuilder, $ignoredServicesRegExp);
+        $serviceDefinitions = $containerBuilder->getDefinitions();
 
-            if ($class === null) {
+        $forcedWires = $containerBuilder->getParameter('kutny_autowiring.forced_wires');
+        foreach ($serviceDefinitions as $serviceId => $definition) {
+            if ($definition->isAbstract() || !$definition->isPublic()) {
                 continue;
             }
 
-            $this->processClass($serviceId, $class, $classes);
-        }
+            if ($definition->getClass() === null) {
+                continue;
+            }
 
-        return $classes;
+            if ($ignoredServicesRegExp && preg_match($ignoredServicesRegExp, $serviceId)) {
+                continue;
+            }
+
+            if ($definition->getFactoryClass() || $definition->getFactoryMethod() || (method_exists($definition, 'getFactory') && $definition->getFactory())) {
+                continue;
+            }
+
+            $this->watchServiceClassForChanges($definition, $containerBuilder);
+
+            $reflection = new ReflectionClass($definition->getClass());
+            $constructor = $reflection->getConstructor();
+
+            if ($constructor !== null && $constructor->isPublic()) {
+                $this->classConstructorFiller->autowireParams($constructor, $serviceId, $definition, $classList, $forcedWires);
+            }
+        }
     }
 
-    private function processClass($serviceId, $class, array &$classes)
+    private function getIgnoredServicesRegExp(ContainerBuilder $containerBuilder)
     {
-        $reflection = new ReflectionClass($class);
+        $ignoredServices = $containerBuilder->getParameter('kutny_autowiring.ignored_services');
 
-        if (!$reflection) {
-            return;
+        if (empty($ignoredServices)) {
+            return null;
         }
 
-        $classes[$reflection->getName()][] = $serviceId;
+        return '~^(' . implode('|', $ignoredServices) . ')$~';
+    }
 
-        foreach ($reflection->getInterfaceNames() as $interface) {
-            $classes[$interface][] = $serviceId;
-        }
+    private function watchServiceClassForChanges(Definition $definition, ContainerBuilder $containerBuilder)
+    {
+        $classReflection = new ReflectionClass($definition->getClass());
 
-        $parent = $reflection;
-
-        while (($parent = $parent->getParentClass())) {
-            $classes[$parent->getName()][] = $serviceId;
-        }
+        do {
+            $containerBuilder->addResource(new FileResource($classReflection->getFileName()));
+        } while ($classReflection = $classReflection->getParentClass());
     }
 }
